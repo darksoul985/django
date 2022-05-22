e68fb4241200d2e13087c33f0b8f4022f6ff5579
Sergey Shirobokov <darksoul345@gmail.com>
2022-05-22 13:58:43 +0300

    завершил 2 домашнее задание

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -19,8 +19,13 @@ import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.index, name='main'),
+    path('contact/', mainapp.contact, name='contact'),
 
-    path('', mainapp.index),
-    path('contact/', mainapp.contact),
-    path('products/', mainapp.products),
+    path('products/', mainapp.products, name='products'),
+    path('products/all/', mainapp.products, name='products_all'),
+    path('products/home/', mainapp.products, name='products_home'),
+    path('products/office/', mainapp.products, name='products_office'),
+    path('products/modern/', mainapp.products, name='products_modern'),
+    path('products/classic/', mainapp.products, name='products_classic'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+import json
+import os as os
+
+
+contact_file = 'contact.json'
+with open(os.path.join(os.getcwd(), contact_file)) as file:
+    contacts = json.load(file)
 
 
 def index(request):
@@ -6,8 +13,22 @@ def index(request):
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'contact': contacts
+    }
+    return render(request, 'mainapp/contact.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'title': 'все'},
+        {'href': 'products_home', 'title': 'дом'},
+        {'href': 'products_office', 'title': 'офис'},
+        {'href': 'products_modern', 'title': 'модерн'},
+        {'href': 'products_classic', 'title': 'классика'},
+    ]
+
+    content = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', content)
