3700b949f9f97b7248b2125507eaeca02e8b0f98
Sergey Shirobokov <darksoul345@gmail.com>
2022-05-26 08:39:09 +0300

    B B B D D peace a lesson lh

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,17 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.index, name='main'),
     path('contact/', mainapp.contact, name='contact'),
-
-    path('products/', mainapp.products, name='products'),
-    path('products/all/', mainapp.products, name='products_all'),
-    path('products/home/', mainapp.products, name='products_home'),
-    path('products/office/', mainapp.products, name='products_office'),
-    path('products/modern/', mainapp.products, name='products_modern'),
-    path('products/classic/', mainapp.products, name='products_classic'),
+    path('products/', include('mainapp.urls', namespace='products')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='название категории', max_length=64,
+                            unique=True)
+    description = models.TextField(verbose_name='краткое описание', blank=True)
+
+    def __str__(self):
+        return f'{self.pk} {self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=128, verbose_name='Название')
+    image = models.ImageField(upload_to='product', blank=True, verbose_name='Изображение')
+    short_desc = models.CharField(max_length=256, verbose_name='Краткое описание')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
