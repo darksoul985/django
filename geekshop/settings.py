9d2ce9af58ad52d5d0f610b8565b62b38d49f291
Sergey Shirobokov <darksoul345@gmail.com>
2022-08-21 13:23:02 +0300

    start lesson_2.3

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'basketapp.apps.BasketappConfig',
     'adminapp.apps.AdminappConfig',
     'social_django',
+    'ordersapp.apps.OrdersappConfig'
 ]
 
 AUTHENTICATION_BACKENDS = (
